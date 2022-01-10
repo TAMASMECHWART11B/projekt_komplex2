6876e0b5efe89f0b7bc482d4c3781e67e0035d66
TAMASMECHWART11B <kallaitamas04@gmail.com>
2022-01-10 19:05:45 +0100

    Alap xmal
    A grafikus elem elhelyezve, kezdetleges kód

---
+++ KomplexProjekt2/Komplex projekt 2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Komplex_projekt_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer idozito = new DispatcherTimer();
+            idozito.Interval = TimeSpan.FromSeconds(0.2);
+            idozito.Tick += new EventHandler(Idozito);
+            Random rnd = new Random();
+            
+            
+            
+        }
+
+        private void Idozito(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
